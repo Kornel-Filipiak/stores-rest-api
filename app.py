91fff43d22b4677221bdb503166517dc73e1cedf
Kornel Filipiak <kornel_filipiak@yahoo.com>
2022-03-30 09:09:22 -0600

    fix db import error

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-from db import db
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
@@ -30,6 +30,7 @@ api.add_resource(UserRegister, "/register")
 
 #prevents flask app from restarting everytime app.py is opened (on import)
 if __name__ == "__main__":
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
     
